a4d96501cc5eacb9d4fa2fe5422bfd5ce99a019a
bouzouitadavid <davidbouzouita@gmail.com>
2018-10-04 16:45:21 +0200

    ajoute des pages et nouveau site

---
+++ restorants.php
@@ -0,0 +1,32 @@
+<?php require "_include/header.html"; ?>
+<body>
+<header>
+    </header>
+    <div class="nav-page"><?php require "_include/nav.html"; ?></div>
+<main class="main-page">
+<div class="row">
+  <div class="col-4">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Charleroi</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Bruxelles</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Namur</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Anvers</a>
+    </div>
+  </div>
+  <div class="col-8">
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+          adresse horaire
+      </div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+    </div>
+  </div>
+</div>
+</main>
+
+<?php require "_include/footer.html"; ?>
+            
+</body>
+</html>
